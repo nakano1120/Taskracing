2bede80d8a5763a945c611e5a170ddeae80e9623
nakano1120 <38690180+nakano1120@users.noreply.github.com>
2019-12-19 14:25:45 +0900

    Add files via upload

--- task.js
+++ task.js
@@ -6,7 +6,7 @@ let target=[60*1000,0,0,0,0,0,0,0]
 let par=[0,0,0,0,0,0,0,0]
 let name=["あなた","さぶろう","とりすけ","もりもり","ことわざ","誰かさん","名前募集中","よろしく"]
 let player=[
-    {id:"1",name:"あなた",target:0,par:0},
+    {id:"1",name:"<span class='red'>あなた</span>",target:0,par:0},
     {id:"2",name:"ジロー",target:0,par:0},
     {id:"3",name:"とりすけ",target:0,par:0},
     {id:"4",name:"たんこぶ",target:0,par:0},
@@ -16,19 +16,22 @@ let player=[
     {id:"8",name:"名前募集中",target:0,par:0}
 ]
 function timer(){
+    if(document.getElementById("time").value<1){
+        alert("時間が不正です")
+        return
+    }
     start = new Date();
     alert("出発します。")
     nowstation = 0
     document.getElementById("form1").style.display = "none";
     document.getElementById("todoname").innerHTML = document.getElementById("todo").value
     rapnumber = document.getElementById("rap").value
-    departuretime = document.getElementById("time").value
+    departuretime = parseInt(document.getElementById("time").value * 60) + parseInt(document.getElementById("sec").value)
     section = [document.getElementById("section1").value,document.getElementById("section2").value,document.getElementById("section3").value] 
     target[0] = parseInt(departuretime) * 60 * 1000
     document.getElementById("next").innerHTML=section[nowstation]
     for( let i=1 ; i<8 ; i++){
-        player[i].target = ((parseInt(departuretime) * 60)+ Math.floor( Math.random() * 60 ) - 35) * 1000
-        console.log(player[i].target)
+        player[i].target = ((parseInt(departuretime))+ Math.floor( Math.random() * 60 ) - 35) * 1000
     }
     
     stationtimer = setInterval(timermain, 100);
@@ -36,6 +39,7 @@ function timer(){
 function checkpoint(){
     if(document.getElementById("form1").style.display == "none"){
         nowstation++
+        document.getElementById("next").innerHTML=section[nowstation]
         player[0].par=Math.floor(nowstation/rapnumber*100)
     }
 }
@@ -57,9 +61,7 @@ function timermain(){
         player[i].target += (Math.floor( Math.random() * 1000 ) - 450);
         player[i].par = (Math.floor(diftime / player[i].target * 100))
         if (player[i].par > 100){
-            player[i].par = 100
         }
-        console.log(player[i].par)
     }
     let playersort = player.slice().sort(function(a,b){
         if(a.par > b.par) return -1;
@@ -69,6 +71,9 @@ function timermain(){
     for (let l=1 ; l<9 ; l++){
         document.getElementById("name"+l).innerHTML=playersort[l-1].name
         document.getElementById("parsent"+l).innerHTML=playersort[l-1].par
+        if(document.getElementById("parsent"+l).innerHTML > 100){
+            document.getElementById("parsent"+l).innerHTML = 100
+        }
     }
     if(nowstation >= rapnumber){
         clearInterval(stationtimer);
